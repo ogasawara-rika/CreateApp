f6313b2d988adf3e57c846bd3f7aeb072bcd6ce9
ogasawara-rika <68369876+ogasawara-rika@users.noreply.github.com>
2022-06-17 16:22:56 +0900

    ヘッダールーティング機能作成

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -8,20 +8,36 @@ import {
 } from "@chakra-ui/react";
 
 type Props = {
+  // 関数名: () => void;とすることで、引数なし返却値なしの関数ということになる
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickUsefManagement: () => void;
+  onClickSetting: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickUsefManagement,
+    onClickSetting
+  } = props;
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">ユーザー一覧</Button>
-            <Button w="100%">設定</Button>
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUsefManagement}>
+              ユーザー一覧
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              設定
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
